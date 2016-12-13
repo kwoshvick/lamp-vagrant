3c9f37a877b02b0ae3476cb9075432aabb5a76d0
kwoshvick <kwoshvick@gmail.com>
2016-12-13 21:01:16 +0300

    added a gitignore file to ignore the .vagrant folder that cobtains the machine files

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # credentials
-PASSWORD = 'toor'
+$PASSWORD = 'password'
 
 # update / upgrade
 sudo apt-get update
@@ -10,17 +10,18 @@ sudo apt-get -y upgrade
 
 #apache set-up
 sudo apt-get install -y apache2
-if ! [ -L /var/www ]; then
-  rm -rf /var/www
-  ln -fs /vagrant /var/www
-fi
+ln -fs /vagrant /var/www/html
+# if ! [ -L /var/www/html ]; then
+#   rm -rf /var/www/html
+#   ln -fs /vagrant /var/www/html
+# fi
 
 #curl
 sudo apt-get install curl
 
 # mysql
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password $PASSWORD'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password $PASSWORD'
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
 sudo apt-get install -y mysql-server
 
 #phpmyadmin
@@ -30,7 +31,7 @@ sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
 sudo apt-get -y install phpmyadmin
-echo " Include /etc/phpmyadmin/apache.conf " >> /etc/apache2/apache2.conf
+echo "Include /etc/phpmyadmin/apache.conf " >> /etc/apache2/apache2.conf
 
 # php 5
 sudo apt-get install php5 php5-cli libapache2-mod-php5 php5-mcrypt
@@ -44,5 +45,3 @@ sudo apt-get -y install git
 # restarting services
 sudo service apache2 restart
 sudo service mysql restart
-
-
