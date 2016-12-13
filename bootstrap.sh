a552ef90fde34c3e011f27d7d59a3d62ee1da0a9
kwoshvick <kwoshvick@gmail.com>
2016-12-13 18:33:51 +0300

    First commit of the Vagrantfile and installation script

---
+++ bootstrap.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# credentials
+PASSWORD = 'toor'
+
+# update / upgrade
+sudo apt-get update
+sudo apt-get -y upgrade
+
+
+#apache set-up
+sudo apt-get install -y apache2
+if ! [ -L /var/www ]; then
+  rm -rf /var/www
+  ln -fs /vagrant /var/www
+fi
+
+#curl
+sudo apt-get install curl
+
+# mysql
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password $PASSWORD'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password $PASSWORD'
+sudo apt-get install -y mysql-server
+
+#phpmyadmin
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+sudo apt-get -y install phpmyadmin
+echo " Include /etc/phpmyadmin/apache.conf " >> /etc/apache2/apache2.conf
+
+# php 5
+sudo apt-get install php5 php5-cli libapache2-mod-php5 php5-mcrypt
+
+# enable mod_rewrite
+sudo a2enmod rewrite
+
+# git
+sudo apt-get -y install git
+
+# restarting services
+sudo service apache2 restart
+sudo service mysql restart
+
+
